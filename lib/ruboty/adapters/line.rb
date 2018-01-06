2b780a2fe8f2d51bfa3c604e24c184a660fca443
manga_osyo <manga.osyo@gmail.com>
2018-01-07 06:18:11 +0900

    Update.

--- lib/ruboty/adapters/line.rb
+++ lib/ruboty/adapters/line.rb
@@ -2,26 +2,25 @@ require "rack"
 require 'line/bot'
 require 'rest-client'
 
-module Line
-  module Bot
-    class HTTPClient
-      def post(url, payload, header = {})
-        Ruboty.logger.debug "======= HTTPClient#post ======="
-        Ruboty.logger.debug "payload #{payload}"
-        Ruboty.logger.debug "FIXIT_URL #{ENV["RUBOTY_FIXIE_URL"]}"
-        RestClient.proxy = ENV["RUBOTY_FIXIE_URL"] if ENV["RUBOTY_FIXIE_URL"]
-        RestClient.post(url, payload, header)
-      end
-    end
-  end
-end
+# module Line
+#   module Bot
+#     class HTTPClient
+#       def post(url, payload, header = {})
+#         Ruboty.logger.debug "======= HTTPClient#post ======="
+#         Ruboty.logger.debug "payload #{payload}"
+#         Ruboty.logger.debug "FIXIT_URL #{ENV["RUBOTY_FIXIE_URL"]}"
+#         RestClient.proxy = ENV["RUBOTY_FIXIE_URL"] if ENV["RUBOTY_FIXIE_URL"]
+#         RestClient.post(url, payload, header)
+#       end
+#     end
+#   end
+# end
 
 
 module Ruboty module Adapters
 	class LINE < Base
-		env :RUBOTY_LINE_CHANNEL_ID,     "YOUR LINE BOT Channel ID"
 		env :RUBOTY_LINE_CHANNEL_SECRET, "YOUR LINE BOT Channel Secret"
-		env :RUBOTY_LINE_CHANNEL_MID,    "YOUR LINE BOT MID"
+		env :RUBOTY_LINE_CHANNEL_TOKEN,  "YOUR LINE BOT Channel token"
 		env :RUBOTY_LINE_ENDPOINT,       "LINE bot endpoint(Callback URL). (e.g. '/ruboty/line'"
 		def run
 			Ruboty.logger.info "======= LINE#run ======="
@@ -31,67 +30,73 @@ module Ruboty module Adapters
 		def say msg
 			Ruboty.logger.info "======= LINE#say ======="
 
+			Ruboty.logger.info "msg : #{msg}"
 			text = msg[:body]
 			to   = msg[:to]
 
 			Ruboty.logger.info "text : #{text}"
 			Ruboty.logger.debug "to : #{to}"
 
-			client.send_text(
-				to_mid: to,
-				text: text
-			)
+			client.reply_message(to, text)
 		end
 
 		private
 		def start_server
-			Rack::Handler::Thin.run(Proc.new{ |env|
+			Rack::Handler::Thin.run(proc { |env|
 				Ruboty.logger.info "======= LINE access ======="
 				Ruboty.logger.debug "env : #{env}"
 
-				request = ::Line::Bot::Receive::Request.new(env)
+				request = Rack::Request.new(env)
+				Ruboty.logger.debug "request : #{request}"
+
 				result = on_post request
 
 				[200, {"Content-Type" => "text/plain"}, [result]]
 			}, { Port: ENV["PORT"] || "8080" })
 		end
 
-		def on_post req
+		def on_post request
 			Ruboty.logger.info "======= LINE#on_post ======="
-			Ruboty.logger.debug "request : #{req}"
 
-			return "OK" unless req.post? && req.fullpath == ENV["RUBOTY_LINE_ENDPOINT"]
+			return "OK" unless request.post? && request.fullpath == ENV["RUBOTY_LINE_ENDPOINT"]
 
-			Ruboty.logger.debug "request.body : #{req.data}"
+			body = request.body.read
+			Ruboty.logger.debug "request.body : #{body}"
 
-			req.data.each { |message|
-				case message.content
-				when ::Line::Bot::Message::Text
-					on_message message
+			events = client.parse_events_from(body)
+			
+
+			events.each { |event|
+				Ruboty.logger.debug "event : #{event}"
+				case event
+				when ::Line::Bot::Event::Message
+					case event.type
+					when ::Line::Bot::Event::MessageType::Text
+						on_message event
+					end
 				end
 			}
 
 			return ""
 		end
 
-		def on_message msg
+		def on_message event
 			Ruboty.logger.info "======= LINE#on_message ======="
-			Ruboty.logger.debug "content : #{msg.content}"
 
 			Thread.start {
 				robot.receive(
-					body: msg.content[:text],
-					from: msg.from_mid,
-					to:   msg.from_mid,
-					message: msg)
+					body: event.message["text"],
+					from: event["replyToken"],
+					to:   event["replyToken"],
+					event: event
+				)
 			}
 		end
 
 		def client
 			@client ||= ::Line::Bot::Client.new { |config|
-				config.channel_id     = ENV["RUBOTY_LINE_CHANNEL_ID"]
 				config.channel_secret = ENV["RUBOTY_LINE_CHANNEL_SECRET"]
-				config.channel_mid    = ENV["RUBOTY_LINE_CHANNEL_MID"]
+				config.channel_token  = ENV["RUBOTY_LINE_CHANNEL_TOKEN"]
  			}
 		end
 	end
